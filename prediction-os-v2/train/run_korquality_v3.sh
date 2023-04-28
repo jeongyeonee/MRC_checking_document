3ecc3886501a78256dcd4d4af703add3b3201226
jylee <ggaggoong97@naver.com>
2023-04-28 06:18:40 +0000

    upload train files

---
+++ prediction-os-v2/train/run_korquality_v3.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+MODEL_TYPE="kobert"
+MODEL_NAME="monologg/kobert"
+DATA_DIR="data/"
+BATCH_SIZE=16
+PRED_FILE="data_valid_230406.json"
+EPOCHS=30
+
+
+TRAIN_FILE="data_train_230406.json"
+OUTPUT_DIR="model/230424/opt7_all_EPOCH30/"
+STEP=100
+#NOANS_PROP=0
+python run_korquality_v3.py --model_type ${MODEL_TYPE} \
+                     --model_name_or_path ${MODEL_NAME} \
+                     --output_dir ${OUTPUT_DIR} \
+                     --data_dir ${DATA_DIR} \
+                     --train_file ${TRAIN_FILE} \
+                     --predict_file ${PRED_FILE} \
+                     --evaluate_during_training \
+                     --per_gpu_train_batch_size ${BATCH_SIZE} \
+                     --per_gpu_eval_batch_size ${BATCH_SIZE} \
+                     --max_seq_length 512 \
+                     --logging_steps ${STEP} \
+                     --save_steps ${STEP} \
+                     --do_train \
+                     --num_train_epochs ${EPOCHS} \
+                     --version_2_with_negative 
+                     #--noans_prop ${NOANS_PROP}
+                     
+TRAIN_FILE="data_train_230406_1by1.json"
+OUTPUT_DIR="model/230424/opt8_1by1_EPOCH30/"
+STEP=50
+#NOANS_PROP=0
+python run_korquality_v3.py --model_type ${MODEL_TYPE} \
+                     --model_name_or_path ${MODEL_NAME} \
+                     --output_dir ${OUTPUT_DIR} \
+                     --data_dir ${DATA_DIR} \
+                     --train_file ${TRAIN_FILE} \
+                     --predict_file ${PRED_FILE} \
+                     --evaluate_during_training \
+                     --per_gpu_train_batch_size ${BATCH_SIZE} \
+                     --per_gpu_eval_batch_size ${BATCH_SIZE} \
+                     --max_seq_length 512 \
+                     --logging_steps ${STEP} \
+                     --save_steps ${STEP} \
+                     --do_train \
+                     --num_train_epochs ${EPOCHS} \
+                     --version_2_with_negative 
+                     #--noans_prop ${NOANS_PROP}
+                     
+TRAIN_FILE="data_train_230406_extract.json"
+OUTPUT_DIR="model/230424/opt9_extract_EPOCH30/"
+STEP=20
+#NOANS_PROP=0
+python run_korquality_v3.py --model_type ${MODEL_TYPE} \
+                     --model_name_or_path ${MODEL_NAME} \
+                     --output_dir ${OUTPUT_DIR} \
+                     --data_dir ${DATA_DIR} \
+                     --train_file ${TRAIN_FILE} \
+                     --predict_file ${PRED_FILE} \
+                     --evaluate_during_training \
+                     --per_gpu_train_batch_size ${BATCH_SIZE} \
+                     --per_gpu_eval_batch_size ${BATCH_SIZE} \
+                     --max_seq_length 512 \
+                     --logging_steps ${STEP} \
+                     --save_steps ${STEP} \
+                     --do_train \
+                     --num_train_epochs ${EPOCHS} \
+                     --version_2_with_negative 
+                     #--noans_prop ${NOANS_PROP}
+                     
+
+
+TRAIN_FILE="data_train_230406.json"
+OUTPUT_DIR="model/230424/opt10_chunk_all_EPOCH30/"
+STEP=100
+python run_korquality_v3.py --model_type ${MODEL_TYPE} \
+                     --model_name_or_path ${MODEL_NAME} \
+                     --output_dir ${OUTPUT_DIR} \
+                     --data_dir ${DATA_DIR} \
+                     --train_file ${TRAIN_FILE} \
+                     --predict_file ${PRED_FILE} \
+                     --evaluate_during_training \
+                     --per_gpu_train_batch_size ${BATCH_SIZE} \
+                     --per_gpu_eval_batch_size ${BATCH_SIZE} \
+                     --max_seq_length 512 \
+                     --logging_steps ${STEP} \
+                     --save_steps ${STEP} \
+                     --do_train \
+                     --num_train_epochs ${EPOCHS} \
+                     --version_2_with_negative \
+                     --noans_prop ${NOANS_PROP}
+                     
+OUTPUT_DIR="model/230424/opt11_chunk_1by1_EPOCH30/"
+STEP=50
+NOANS_PROP=1
+python run_korquality_v3.py --model_type ${MODEL_TYPE} \
+                     --model_name_or_path ${MODEL_NAME} \
+                     --output_dir ${OUTPUT_DIR} \
+                     --data_dir ${DATA_DIR} \
+                     --train_file ${TRAIN_FILE} \
+                     --predict_file ${PRED_FILE} \
+                     --evaluate_during_training \
+                     --per_gpu_train_batch_size ${BATCH_SIZE} \
+                     --per_gpu_eval_batch_size ${BATCH_SIZE} \
+                     --max_seq_length 512 \
+                     --logging_steps ${STEP} \
+                     --save_steps ${STEP} \
+                     --do_train \
+                     --num_train_epochs ${EPOCHS} \
+                     --version_2_with_negative \
+                     --noans_prop ${NOANS_PROP}
+                     
+OUTPUT_DIR="model/230424/opt12_chunk_extract_EPOCH30/"
+STEP=20
+NOANS_PROP=0
+python run_korquality_v3.py --model_type ${MODEL_TYPE} \
+                     --model_name_or_path ${MODEL_NAME} \
+                     --output_dir ${OUTPUT_DIR} \
+                     --data_dir ${DATA_DIR} \
+                     --train_file ${TRAIN_FILE} \
+                     --predict_file ${PRED_FILE} \
+                     --evaluate_during_training \
+                     --per_gpu_train_batch_size ${BATCH_SIZE} \
+                     --per_gpu_eval_batch_size ${BATCH_SIZE} \
+                     --max_seq_length 512 \
+                     --logging_steps ${STEP} \
+                     --save_steps ${STEP} \
+                     --do_train \
+                     --num_train_epochs ${EPOCHS} \
+                     --version_2_with_negative \
+                     --noans_prop ${NOANS_PROP}
